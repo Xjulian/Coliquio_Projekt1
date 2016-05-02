8af2b74a75e129641af3af22f725e6a744b2b20f
Xjulian <juliansachs@gmx.de>
2016-05-02 17:03:58 +0200

    DOM Manipulation JS
    Auf Seite 1, linke Spalte. (Responsive update)

---
+++ design/dom.js
@@ -0,0 +1,17 @@
+function dom(){
+  var ergaenzung = document.getElementById("liste1").childNodes[0];
+  var ergaenzung_erweitert = ergaenzung.nodeValue + " 1";
+  ergaenzung.nodeValue = ergaenzung_erweitert;
+  var ergaenzung = document.getElementById("liste2").childNodes[0];
+  var ergaenzung_erweitert = ergaenzung.nodeValue + " 2";
+  ergaenzung.nodeValue = ergaenzung_erweitert;
+  var ergaenzung = document.getElementById("liste3").childNodes[0];
+  var ergaenzung_erweitert = ergaenzung.nodeValue + " 3";
+  ergaenzung.nodeValue = ergaenzung_erweitert;
+  var ergaenzung = document.getElementById("liste4").childNodes[0];
+  var ergaenzung_erweitert = ergaenzung.nodeValue + " 4";
+  ergaenzung.nodeValue = ergaenzung_erweitert;
+//  if(document.getElementById('button()').onclick == true){
+//  alert("adhgsgl");}
+//  Funktioniert nicht!
+}
